d17ad60dd953fa912078bcb23c06acd8ee826a7e
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-10-12 20:27:16 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [visi,setvisi]=useState({display:"none"})
+  const para=()=>{
+    setvisi({display:"block"})
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={para} id="click">Show</button>
+      <p id="para"style={visi}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </div>
   );
 }
